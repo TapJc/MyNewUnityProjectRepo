67ac21ec7cc0619ef869597faab6a84407c00d17
TapJc <112350012+TapJc@users.noreply.github.com>
2022-09-02 14:01:24 -0500

    Added Challenge 1

---
+++ Challenge1/Assets/Challenge 1/Scripts/LoseOutOfBounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseOutOfBounds : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -51 || transform.position.y > 80)
+        {
+            ScoreManager.gameOver = true;
+        }
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    // Set this in the Inspector 
+    public Text textbook;
+
+    // Initializes/Updates varibles when restarting
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbook.text = "Score: " + score;
+        }
+
+        // Win Condition: 5 or more points
+        if (score >= 5)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbook.text = "You win!\nPress R to Try Again!";
+            }
+            else
+            {
+                textbook.text = "You lose!\nPress R to Try Again!";
+            }
+
+            // Restarts Game
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
