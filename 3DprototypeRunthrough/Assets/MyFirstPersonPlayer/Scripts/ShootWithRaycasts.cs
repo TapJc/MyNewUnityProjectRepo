fe446377974758afc693333e9b3c7503997955de
TapJc <112350012+TapJc@users.noreply.github.com>
2022-10-22 09:08:38 -0500

    Finished Assignment 5B 3D

---
+++ 3DprototypeRunthrough/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Brian Sida
+ * Assignment 5B
+ * Description: Handles weapon mechanics
+*/
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    public float hitForce = 10f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+
+        // if we hit something with out ray..
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            // get the target script off the hit object
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            // if the target script is found, make the target take damage
+            if (target != null) 
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ 3DprototypeRunthrough/Assets/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Brian Sida
+ * Assignment 5B
+ * Description: Creates health and die methods for enemies 
+*/
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
